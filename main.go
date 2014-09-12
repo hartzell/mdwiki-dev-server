86b591654d096e81d97a6b458960e8f24e9de637
George Hartzell <hartzell@alerce.com>
2014-09-12 14:36:32 -0700

    Add ability to specify content directory.
    Add a "-d" option that specifies the directory from which the content
    will be served.  Default is "./".

--- main.go
+++ main.go
@@ -1,11 +1,16 @@
 package main
 
 import (
+	"flag"
 	"log"
 	"net/http"
 )
 
+var flagDir =
+	flag.String("d", "./", "Directory from which to read files")
+
 func main() {
-	http.Handle("/", http.FileServer(http.Dir("/Users/hartzell/tmp/foo")))
+	flag.Parse();
+	http.Handle("/", http.FileServer(http.Dir(*flagDir)))
 	log.Fatal(http.ListenAndServe(":8080", nil))
 }
