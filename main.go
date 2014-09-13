380710ed792d851ceffcc5bdad478c44cb17abb9
George Hartzell <hartzell@alerce.com>
2014-09-12 17:26:18 -0700

    Hook up watcher/notifier server side stuff.
    Based on https://github.com/rowoot/node-live-reload.
    
    Hook up a websocket handler that watches for changes in the the content
    dir and sends a notification to the client.
    
    Filesystem changes seem to "bounce" (saving a file causes several
    notifications).  Unclear what's up, some of it might be spotlight
    activity.
    
    Changes are filtered against a regular expression, user settable via a
    command line option (-r).  Default is ".*(md|html|css)$".
    
    Sends a json encoded message that looks more or less like {"r": "time"}
    where the time is the time the server handled the fsnotify event (aka
    time.Now()).

--- main.go
+++ main.go
@@ -1,16 +1,73 @@
 package main
 
 import (
+	"net/http"
+
+	"code.google.com/p/go.net/websocket"
+	"encoding/json"
+	"gopkg.in/fsnotify.v1"
+
 	"flag"
 	"log"
-	"net/http"
+	"regexp"
+	"time"
 )
 
-var flagDir =
-	flag.String("d", "./", "Directory from which to read files")
+var (
+	flagContentDir = flag.String("d", "./",
+		"Directory from which to read files")
+	flagNotifyRegexp = flag.String("r", ".*(md|html|css)$",
+		"Regular expression that matches files to watch for changes")
+)
+
+func ok(err error) {
+	if err != nil {
+		log.Fatal(err)
+	}
+}
+
+func webHandler(ws *websocket.Conn) {
+	type ReloadMessage struct {
+		R time.Time `json:"r"`
+	}
+
+	watcher, err := fsnotify.NewWatcher()
+	ok(err)
+
+	defer watcher.Close()
+
+	err = watcher.Add(*flagContentDir)
+	ok(err)
+
+	for {
+		select {
+		case event := <-watcher.Events:
+			matched, err :=
+				regexp.MatchString(*flagNotifyRegexp, event.Name)
+			ok(err)
+
+			if !matched || event.Op&fsnotify.Chmod == fsnotify.Chmod {
+				continue
+			}
+
+			message := ReloadMessage{R: time.Now()}
+			b, err := json.Marshal(message)
+			ok(err)
+
+			log.Println("_reload sent because:", event)
+			websocket.Message.Send(ws, string(b))
+
+		case err := <-watcher.Errors:
+			log.Println("error:", err)
+		}
+	}
+}
 
 func main() {
-	flag.Parse();
-	http.Handle("/", http.FileServer(http.Dir(*flagDir)))
+	flag.Parse()
+
+	http.Handle("/_reloader", websocket.Handler(webHandler))
+	http.Handle("/", http.FileServer(http.Dir(*flagContentDir)))
+
 	log.Fatal(http.ListenAndServe(":8080", nil))
 }
